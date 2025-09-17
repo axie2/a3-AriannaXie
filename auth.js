98ccbb4f88a06e7de220c9d030ff43815355bbaa
Arianna Xie <ariannaxie7@gmail.com>
2025-09-16 23:31:43 -0400

    Added full path of callback.

--- auth.js
+++ auth.js
@@ -5,6 +5,9 @@ var qs = require("querystring");
 var router = express.Router();
 
 const isProduction = process.env.NODE_ENV === "production";
+const CALLBACK_URL = isProduction
+    ? "https://a3-ariannaxie.onrender.com/oauth2/redirect"
+    : "http://localhost:3000/oauth2/redirect";
 const RETURN_TO_URL = isProduction
     ? "https://a3-ariannaxie.onrender.com/login"
     : "http://localhost:3000/login";
@@ -19,7 +22,7 @@ passport.use(
             userInfoURL: "https://" + process.env["AUTH0_DOMAIN"] + "/userinfo",
             clientID: process.env["AUTH0_CLIENT_ID"],
             clientSecret: process.env["AUTH0_CLIENT_SECRET"],
-            callbackURL: process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/oauth2/redirect",
+            callbackURL: CALLBACK_URL,
             scope: ["profile"],
         },
         function verify(issuer, profile, cb) {
